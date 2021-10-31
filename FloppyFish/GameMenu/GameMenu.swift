8af671243f90f8cb2f6cd1f989d5d420e54fb9e2
Colin Morrison <colinjmmorris@gmail.com>
2021-10-31 14:36:38 +0000

    GameMenu setup with working play button label

---
+++ FloppyFish/GameMenu/GameMenu.swift
@@ -0,0 +1,40 @@
+//
+//  GameScene.swift
+//  FloppyFish
+//
+//  Created by Colin Morrison on 31/10/2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameMenu: SKScene {
+    
+//    private var label : SKLabelNode?
+    
+    override func didMove(to view: SKView) {
+        
+//        self.label = self.childNode(withName: "//titleLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            
+            switch atPoint(touchLocation).name {
+
+            case "playLabel":
+                let gameScene = SKScene(fileNamed: "GameScene")!
+                gameScene.scaleMode = .aspectFill
+                view?.presentScene(gameScene, transition: SKTransition.fade(withDuration: 0.9))
+
+            default: break
+
+            }
+        }
+    }
+}
